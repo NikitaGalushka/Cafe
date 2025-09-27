d49e438905c870c08fd4ba8bba2d2447a8ba3bd4
NikitaGalushka <nikitagaluska593@gmail.com>
2025-09-27 15:37:14 +0300

    Add files via upload

--- Kafe4.0/Cafe/models.py
+++ Kafe4.0/Cafe/models.py
@@ -19,9 +19,16 @@ class Cafe(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items", null=True, blank=True)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name="items",
+        null=True,
+        blank=True
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- Kafe4.0/kafe_project/urls.py
+++ Kafe4.0/kafe_project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from Cafe import views
 
 urlpatterns = [
@@ -17,3 +19,6 @@ urlpatterns = [
     path('change_password/', include('Cafe.urls')),
     path('payment/', include('Cafe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
